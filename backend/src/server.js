0aa755197bcfe5b4a159c09df53b52eca5a18c44
bishwaschhantyal <bishwaschhantyal4@gmail.com>
2026-03-13 14:22:46 +0545

    small changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,17 +5,17 @@ import authRoute from "./modules/auth/auth.routes.js";
 import connectDb from "./config/db.js";
 import otpRoute from "./modules/otp/otp.route.js";
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 const app = express();
 
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	}),
 );
 
 app.use(express.json());
@@ -28,7 +28,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/otp", otpRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
 export default app;
